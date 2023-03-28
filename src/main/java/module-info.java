18f0eba62b9530b6994f2d7715e3d3c1e4a0375f
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-03-28 08:44:20 +0200

    ko2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,7 @@ module com.example.devops {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-
-
     opens com.example.devops to javafx.fxml;
+
     exports com.example.devops;
 }
