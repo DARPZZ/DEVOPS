73db968b5c1a4b67cc8531c758f34fdd9642e340
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-03-28 07:36:05 +0200

    ko

--- src/main/java/com/example/devops/HelloApplication.java
+++ src/main/java/com/example/devops/HelloApplication.java
@@ -13,8 +13,9 @@ public class HelloApplication extends Application
     public void start(Stage stage) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1350, 800);
         stage.setTitle("Hello!");
+
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.devops {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.devops to javafx.fxml;
